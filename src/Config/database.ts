9349ea36bda1fc55fb20e8abb9dced5944b0c609
Rayel <rayelnabie@gmail.com>
2026-04-29 12:31:58 +0200

    feat: Split getsecrets to own file for seperation of concerns

--- src/Config/database.ts
+++ src/Config/database.ts
@@ -1,48 +1,4 @@
-import fs from 'node:fs';
-import path from 'node:path';
-
-const getSecret: (
-  secretName: string,
-  fallback: string,
-) => {
-  value: string;
-  source: string;
-} = (secretName: string, fallback: string): { value: string; source: string } => {
-  const dockerSecretPath: string = path.join('/run/secrets', secretName);
-  const localSecretPath: string = path.join(process.cwd(), 'secrets', `${secretName}.txt`);
-
-  /**
-   * Try docker secrets
-   **/
-  try {
-    if (fs.existsSync(dockerSecretPath)) {
-      return { value: fs.readFileSync(dockerSecretPath, 'utf8').trim(), source: 'docker-secret' };
-    }
-  } catch {
-    // Silence error
-  }
-
-  /**
-   * Try the local secret file
-   **/
-  try {
-    if (fs.existsSync(localSecretPath)) {
-      return { value: fs.readFileSync(localSecretPath, 'utf8').trim(), source: 'local-file' };
-    }
-  } catch {
-    // Silence error
-  }
-
-  /**
-   * Fallback to env variable
-   * **/
-  const envVarName: string = secretName.toUpperCase();
-  if (process.env[envVarName]) {
-    return { value: process.env[envVarName] as string, source: 'env-var' };
-  }
-
-  return { value: fallback, source: 'default' };
-};
+import { getSecret } from '#Utils/secrets.js';
 
 export const databaseConfig = {
   get details(): { url: string; source: string } {
